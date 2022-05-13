3cbfdccc6ce0b5e47f6a49feb06ebc7ec2ed4269
Krzysztof Siuda <kksiuda@gmail.com>
2022-05-13 08:34:46 +0100

    added string | Error as default type

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,10 +1,10 @@
 export type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'silent';
 
-export interface LogMessage<T = string> {
+export interface LogMessage<T = string | Error> {
   namespace: string;
   timestamp: number;
   logLevel: Exclude<LogLevel, 'silent'>;
   payload: T;
 }
 
-export type LogConsumer<T = string> = (msg: LogMessage<T>) => any;
+export type LogConsumer<T = string | Error> = (msg: LogMessage<T>) => any;
